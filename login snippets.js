a23387159a27a0d3c63cb08e69283a83a2656235
Jolivé Hodehou <49763191+JoliveHodehou@users.noreply.github.com>
2020-06-29 17:42:31 +0100

    Create login snippets.js

---
+++ login snippets.js
@@ -0,0 +1,6 @@
+        document.querySelector("#login_field").value="test@test.com"
+        document.querySelector("#password").value="test"
+        document.querySelector("input.btn.btn-primary.btn-block").click()
+        document.querySelector("input.btn.btn-primary.btn-block").dispatchEvent(new Event('submit' , {'bubbles':true}))
+
+

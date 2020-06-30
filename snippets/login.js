91f9bfdeeaf01b7ddd8a1ea943df578b0baa715c
Jolivé Hodehou <49763191+JoliveHodehou@users.noreply.github.com>
2020-06-30 16:51:08 +0100

    Delete login.js

--- snippets/login.js
+++
@@ -1,7 +0,0 @@
-        document.querySelector("#login_field").value="test@test.com"
-        document.querySelector("#password").value="test"
-        document.querySelector("input.btn.btn-primary.btn-block").click()
-        document.querySelector("input.btn.btn-primary.btn-block").dispatchEvent(new Event('submit' , {'bubbles':true}))
-
-
-
